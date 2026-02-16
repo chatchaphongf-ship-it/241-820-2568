164b9031b06c7fa933a5c18777935ea7247b8302
chatchaphongf-ship-it <chatchaphong.f@ku.th>
2026-02-16 16:47:47 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,71 @@
-function submitData() {
-    let firstNameDOM = document.querySelector('input[name=firstname]');
-    let lastNameDOM = document.querySelector('input[name=lastname]');
-    let ageDOM = document.querySelector('input[name=age]');
-    let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestDOM = document.querySelectorAll('input[name=interest]:checked');
-    let descriptionDOM = document.querySelector('textarea[name=description]');
+const express = require('express');
+const bodyParser = require('body-parser');
+const e = require('express');
+const app = express();
+const port = 8000;
 
-    let interest = ''
-    for (let i = 0; i < interestDOM.length; i++) {
-        interest += interestDOM[i].value
-        if (i != interestDOM.length - 1) {
-            interest += ', '
+app.use(bodyParser.json());
+
+let users = []
+let counter = 1;
+
+//part = GET /users
+app.get('/users',(req, res)=>{
+    res.json(users);
+});
+
+//path = POST /user
+app.post('/user',(req, res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1
+    users.push(user);
+    res.json({
+        message: 'User added successfully',user: user});
+})
+
+//path = PUT /user/:id
+app.put('/user/:id',(req, res)=>{
+    let id = req.params.id
+    let updatedUser = req.body
+    //หา users จาก id
+    let selectedIndex = users.findIndex(user => user.id == id)
+
+    //update users นั้น
+    if (update)
+    users[selectedIndex].name = updatedUser.name || user[selectedIndex].name;
+    users[selectedIndex].email = updatedUser.email || user[selectedIndex].email;
+    users[selectedIndex].age = updatedUser.age || user[selectedIndex].age;
+
+    //ส่ง response กลับไปว่า update users ที่เลือกสำเร็จแล้ว
+    res.json({
+        message: 'User updated succressfully',
+        data: {
+            user: updatedUser,
+            indexUpdated:selectedIndex
         }
+    })
+})
+
+//path = DELETE /users/:id
+app.delete('/users/:id',(req, res) => {
+    let id = req.params.id
+    let selectedIndex = users.findIndex(user => user.id == id)
+    if (selectedIndex !== -1){
+        users.splice(selectedIndex,1)
+        res.json({
+            message:'User deleted successfully',
+            data: {
+                indexDeleted: selectedIndex
+            }
+        })
+    }else{
+        res.status(404).json({
+            message: 'User not found'
+        })
     }
-    
-    let userData = {
-        firstName: firstNameDOM.value,
-        lastName: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-    }
-    console.log('submit data',userData);
-}
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
